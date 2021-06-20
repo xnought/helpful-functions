d384673891169b525290464f7c1c697f018c9219
xnought <bertuccd@oregonstate.edu>
2021-06-20 14:36:36 -0700

    added first function to test

---
+++ src/index.ts
@@ -0,0 +1,7 @@
+/**
+ * Function that returns if the array is empty or not
+ *
+ * @param array given any type of array
+ * @returns true or false answering the question -> isEmpty?
+ */
+const isEmpty = (array: any[]): boolean => !(array.length > 0);
