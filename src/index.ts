2bebe4f16ff36d81ae39cd51ac0ced8f30daaf45
xnought <bertuccd@oregonstate.edu>
2021-06-20 18:32:16 -0700

    added some colors and preview

--- src/index.ts
+++ src/index.ts
@@ -17,6 +17,34 @@ export const isEmpty = <T>(array: T[]): boolean => !(array.length > 0);
 export const copyArray = <T>(array: T[]): T[] =>
 	JSON.parse(JSON.stringify(array));
 
+/**
+ * 10 good colors that work together
+ */
+export let scheme1 = [
+	"#001219",
+	"#005f73",
+	"#0a9396",
+	"#94d2bd",
+	"#e9d8a6",
+	"#ee9b00",
+	"#ca6702",
+	"#bb3e03",
+	"#ae2012",
+	"#9b2226",
+];
+
+/**
+ * This prints the string with the colors in the console
+ *
+ * @param array of colors
+ *
+ */
+export const logColors = (arrayOfColors: string[]): void => {
+	arrayOfColors.forEach((color) => {
+		console.log(`%c ${color}`, `color: ${color}`);
+	});
+};
+
 // const main = () => {
 // };
 // main();
